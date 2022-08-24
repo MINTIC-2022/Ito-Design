cefc9aada177e290d131108c17e9a71144bc2694
jhonhc@hotmail.es <jhonhc@hotmail.es>
2022-08-24 16:20:25 -0500

    Arbol base para proyecto Empresa en Sprint boot- se cargo lombok

---
+++ itoDesignProyectoEmpresa/proyectoEmpresa/src/main/java/itoDesign/proyectoEmpresa/ProyectoEmpresaApplication.java
@@ -0,0 +1,13 @@
+package itoDesign.proyectoEmpresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEmpresaApplication.class, args);
+	}
+
+}
